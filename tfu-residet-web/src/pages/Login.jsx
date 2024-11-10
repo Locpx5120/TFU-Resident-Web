c4f824d0e5085dfe156938e3df3241f0731aca78
Bui Tien Anh <anhbthe170035@fpt.edu.vn>
2024-11-10 15:39:40 +0700

    refactor: change api call

--- tfu-residet-web/src/pages/Login.jsx
+++ tfu-residet-web/src/pages/Login.jsx
@@ -1,10 +1,11 @@
-import React, { useContext, useState } from "react";
-import { Container, Row, Col, Form, FormGroup, Button } from "react-bootstrap";
+import React, {useContext, useState} from "react";
+import {Container, Row, Col, Form, FormGroup, Button} from "react-bootstrap";
 import '../styles/login.css';
-import { Link, useNavigate } from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import Swal from "sweetalert2";
-import { authService } from "../services/authService";
+import {authService} from "../services/authService";
 import Cookies from "js-cookie";
+import {postData} from "../services/api";
 
 const Login = () => {
     const [credentials, setCredentials] = useState({
@@ -12,32 +13,25 @@ const Login = () => {
         password: ''
     });
 
-    const { dispatch } = useContext(authService);
+    const {dispatch} = useContext(authService);
     const navigate = useNavigate();
 
     // Hàm thay đổi giá trị input
     const handleChange = e => {
-        setCredentials(prev => ({ ...prev, [e.target.id]: e.target.value }));
+        setCredentials(prev => ({...prev, [e.target.id]: e.target.value}));
     };
 
     // Hàm xử lý đăng nhập
     const handleClick = async e => {
         e.preventDefault();
-        dispatch({ type: 'LOGIN_START' });
+        dispatch({type: 'LOGIN_START'});
 
         try {
-            const res = await fetch('http://localhost:5045/api/auth/token', {
-                method: 'POST',
-                headers: {
-                    'content-type': 'application/json'
-                },
-                body: JSON.stringify(credentials)
-            })
-
+            const res = await postData('/auth/token', credentials)
             const result = await res.json();
             if (!res.ok) alert(result.message)
             if (result.data && result.data.token) {
-                Cookies.set('accessToken', result.data.token, { expires: 1 });
+                Cookies.set('accessToken', result.data.token, {expires: 1});
 
                 const isNew = localStorage.getItem('isNew') || false;
 
@@ -45,7 +39,7 @@ const Login = () => {
                     navigate('/change-password');
                     return;
                 }
-                dispatch({ type: "LOGIN_SUCCESS", payload: result.data.user });
+                dispatch({type: "LOGIN_SUCCESS", payload: result.data.user});
 
                 Swal.fire({
                     icon: 'success',
@@ -59,7 +53,7 @@ const Login = () => {
                 navigate('/');
             }
         } catch (error) {
-            dispatch({ type: "LOGIN_FAILURE", payload: error.response?.data?.message || error.message });
+            dispatch({type: "LOGIN_FAILURE", payload: error.response?.data?.message || error.message});
 
             Swal.fire({
                 icon: 'error',
@@ -83,10 +77,12 @@ const Login = () => {
 
                                 <Form onSubmit={handleClick}>
                                     <FormGroup>
-                                        <input type="email" placeholder='Email' id='email' onChange={handleChange} required />
+                                        <input type="email" placeholder='Email' id='email' onChange={handleChange}
+                                               required/>
                                     </FormGroup>
                                     <FormGroup>
-                                        <input type="password" placeholder='Mật khẩu' id='password' onChange={handleChange} required />
+                                        <input type="password" placeholder='Mật khẩu' id='password'
+                                               onChange={handleChange} required/>
                                     </FormGroup>
                                     <Button className='btn primary-btn' type='submit'>Đăng nhập</Button>
                                 </Form>
--- tfu-residet-web/src/services/buildingService.js
+++
@@ -1,1 +0,0 @@
-//API Liên quan đến TÒA NHÀ
