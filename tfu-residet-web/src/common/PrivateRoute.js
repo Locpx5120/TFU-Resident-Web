e7cc8448a8ab9e28f39776f56897e6c3e7c78baa
Bui Tien Anh <anhbthe170035@fpt.edu.vn>
2024-11-11 19:09:11 +0700

    refactor: divine routing by hostname

--- tfu-residet-web/src/common/PrivateRoute.js
+++ tfu-residet-web/src/common/PrivateRoute.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import {Navigate, Outlet} from 'react-router-dom';
 import Cookies from 'js-cookie';
 
+const BUILDING_HOST = 'localhost';
+const PRODUCT_HOST = 'producthost';
 const PrivateRoute = () => {
     const isAuthenticated = Cookies.get("accessToken");
-    return !!isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+    return !!isAuthenticated ? <Outlet/> : window.location.hostname !== BUILDING_HOST ?
+        <Navigate to={`/login-building/` + Cookies.get('buildingID')}/> : <Navigate to="/login"/>;
 };
 
 export default PrivateRoute;
