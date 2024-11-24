9e6274137088d130c72c6ca6069b16b6727b8513
Bui Tien Anh <anhbthe170035@fpt.edu.vn>
2024-11-24 13:29:17 +0700

    fix: request

--- tfu-residet-web/src/index.js
+++ tfu-residet-web/src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthServiceProvider } from './services/authService';
+import {PrimeReactProvider} from "primereact/api";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthServiceProvider><App /></AuthServiceProvider>
+  <PrimeReactProvider> <AuthServiceProvider><App /></AuthServiceProvider></PrimeReactProvider>
   </React.StrictMode>
 );
 
